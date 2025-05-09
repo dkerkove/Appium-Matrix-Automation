c77521768ef1391498b6d130d79a7056fb6dd8b3
dkerkove <dankerkove@gmail.com>
2025-05-09 11:09:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,53 @@ try:
 except KeyError:
     API_KEY = "Token not available!"
 
+HOST_NAME = "https://jedi.enterprise.corellium.com"
+INSTANCE_ID = ""
+
+
 
 
 def instance():
     headers = {
         'Authorization': f"Bearer {API_KEY}"
     }
-    r = requests.get("https://jedi.enterprise.corellium.com/api/v1/instances", headers=headers)
+    r = requests.get(f"{HOST_NAME}/api/v1/instances", headers=headers)
     if r.status_code == 200:
         data = r.json()
         device_name = data[0]['name']
         print(f'Name of first device: {device_name}')
+
+
+def VMReadyCheck():
+    headers = {
+        'Authorization': f"Bearer {API_KEY}"
+    }
+    r = requests.get(f"{HOST_NAME}/api/v1/instances/{INSTANCE_ID}/agent/v1/app/ready", headers=headers)
+    if r.status_code == 200:
+        data = r.json()
+        print(f"VM Ready: {data['ready']}")
+
+
+def installApp(APP_NAME):
+    headers = {
+        'Authorization': f"Bearer {API_KEY}"
+    }
+    data = {
+        "path": f"/tmp/{APP_NAME}"
+    }
+    r = requests.POST(f"{HOST_NAME}/api/v1/instances/{INSTANCE_ID}/agent/v1/app/install", headers=headers, data=data)
+    if r.status_code == 200:
+        print(f"{APP_NAME} installing ...")
+
+
+def startApp(BUNDLE_ID):
+    headers = {
+        'Authorization': f"Bearer {API_KEY}"
+    }
+    r = requests.POST(f"{HOST_NAME}/api/v1/instances/{INSTANCE_ID}/agent/v1/app/apps/{BUNDLE_ID}/run", headers=headers)
+    if r.status_code == 200:
+        print(f"{BUNDLE_ID} started ...")
+
+
+
+    
