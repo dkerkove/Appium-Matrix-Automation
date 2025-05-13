bbd8b28468b9cb7d3e403bd066ba25e9b917a6d7
dkerkove <dankerkove@gmail.com>
2025-05-13 11:37:07 -0500

    Update appium_cafe.py

--- appium_cafe.py
+++ appium_cafe.py
@@ -10,7 +10,7 @@ def automate_app():
     options.automation_name = 'XCUITest'
     
     # Unique to device
-    options.udid = '00008120B14A00BB75CCBA77' # UDID
+    # options.udid = '00008120B14A00BB75CCBA77' # UDID
     options.device_name = "faithful-medieval" # Name
     options.platform_name = 'iOS'
     options.platform_version = '18.3.1' # iOS version
